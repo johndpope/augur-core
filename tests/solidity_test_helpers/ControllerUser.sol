c02ec8734060bea39ee7355a1b3fda9727b21fd1
Micah Zoltu <micah@zoltu.net>
2017-09-17 12:27:06 -0700

    Deletes collection libraries, topics and remaining Serpent code.
    With the decision to require an Augur node, Topics can be deleted.  This has a cascading effect of allowing us to delete some other stuff, including some collection libraries.  In the process, I deleted any collection libraries that were unused and migrated the single remaining used collection library over to Solidity (in the form of a struct with extensions).
    
    Migrates the placeholder ReportingEscapeHatch to Solidity.
    
    Removes the last of the solidity code (some macros and miscellaneous tests).

---
+++ tests/solidity_test_helpers/ControllerUser.sol
@@ -0,0 +1,17 @@
+pragma solidity ^0.4.13;
+
+import 'ROOT/IControlled.sol';
+
+
+contract ControllerUser is IControlled {
+    address public suicideFundsDestination;
+    address public updatedController;
+
+    function setController(IController _controller) public returns(bool) {
+        updatedController = _controller;
+    }
+
+    function suicideFunds(address _destination) public returns (bool) {
+        suicideFundsDestination = _destination;
+    }
+}
